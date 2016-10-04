3c5f0a432cab713c9f9639e50577cf49ca99518f
Philip Potter <philip.g.potter@gmail.com>
2016-10-04 20:14:25 +0100

    first test: empty logs

--- tests/test_verifiable_log.py
+++ tests/test_verifiable_log.py
@@ -9,21 +9,14 @@ Tests for `verifiable_log` module.
 """
 
 import pytest
+from binascii import hexlify
+
+from verifiable_log.verifiable_log import VerifiableLog
 
 
-from verifiable_log import verifiable_log
+EMPTY_HASH=b'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
-
-class TestVerifiable_log(object):
-
-    @classmethod
-    def setup_class(cls):
-        pass
-
-    def test_something(self):
-        pass
-
-    @classmethod
-    def teardown_class(cls):
-        pass
+def test_expected_empty_log_hash():
+    vlog = VerifiableLog()
+    assert hexlify(vlog.currentRoot()) == EMPTY_HASH
 
--- verifiable_log/verifiable_log.py
+++ verifiable_log/verifiable_log.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from hashlib import sha256
+
+class VerifiableLog(object):
+    def currentRoot(self):
+        return sha256(b'').digest()
